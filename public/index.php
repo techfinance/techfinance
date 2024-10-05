c681d40f343ca0cc2068d26e7301c61d46217092
Guilherme <guila_cid@hotmail.com>
2024-10-04 22:28:05 -0300

    head navbar
    primeira commit do desenvolvimento frontend. navbar construida

---
+++ public/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechFinance</title>
+
+    <!-- Fontes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <head>
+        <div class="container-fluid top-nav">
+            <div class="navbar-brand"><img src="images/logo.jpg" alt="logo do site" id="logo"></div>
+            <div class="nav-config">
+                <i class="bi bi-gear nav-config-items" id="config"></i>
+                <i class="bi bi-person-circle nav-config-items" id="user"></i>
+            </div>
+        </div>
+        <nav class="navbar navbar-expand-lg" id="nav-bar">
+            <div class="container-fluid" id="container-nav">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Registros de Despesas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Metas Financeiras</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sonhos de Consumo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Meus Gastos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </head>
+
+    
+
+</body>
+</html>
