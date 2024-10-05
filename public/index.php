dedcf1f5fa1045b7b13512157d0f654263e7b4b4
Guilherme <guila_cid@hotmail.com>
2024-10-05 07:25:29 -0300

    Organização do CSS e layout home

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/navbar.css">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
@@ -57,6 +57,16 @@
             </div>
         </nav>
     </head>
+    <main>
+
+
+    </main>
+    <footer>
+        <div class="container-fluid" id="footer">
+            &copy; All rights reserved | TechFinance 2024. Todos os direitos reservados. 
+        </div>
+    </footer>
+
 
     
 
