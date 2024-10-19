9ea08690dac094a9c9690549d509443934eaf8a5
Guilherme <guila_cid@hotmail.com>
2024-10-19 09:20:39 -0300

    Criação da classe Categoria

---
+++ src/models/Categoria.php
@@ -0,0 +1,25 @@
+<?php
+    class Categoria extends Banco {
+        
+        public function setCategoriau($nome_categoria, $id_usuario){
+            $sql = $this->pdo->prepare("INSERT INTO categoriau (NOME_CATEGORIAU, Usuario_ID_USUARIO) VALUES (:n, :i)");
+            $sql->bindValue(":n", $nome_categoria);
+            $sql->bindValue(":i", $id_usuario);
+            $sql->execute();
+
+            return true;
+        }
+
+        public function getAllCategoria($id_usuario){
+            $dados = array();
+            $sql = $this->pdo->query("SELECT id_categoriau id, nome_categoriau nome, usuario_id_usuario FROM categoriau WHERE usuario_id_usuario = $id_usuario UNION ALL SELECT id_categoria id, nome_categoria nome, null FROM categoria");
+            $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+            return $dados;
+        }
+
+
+    }
+
+
+?>
